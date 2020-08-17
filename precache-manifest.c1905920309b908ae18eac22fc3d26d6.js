a08fdda4b5d41fede85a5c4ed32df1ce8b3bdea6
Oleksandr Dashevskyi <dashevskyi2013@gmail.com>
2020-08-16 17:55:41 -0700

    Updates

---
+++ precache-manifest.c1905920309b908ae18eac22fc3d26d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ba9ee74dd82fbbc4572d565de27e88",
+    "url": "/todo_list_updated-axios/index.html"
+  },
+  {
+    "revision": "9df10b8695ae6e3746a4",
+    "url": "/todo_list_updated-axios/static/css/main.faf3ddbb.chunk.css"
+  },
+  {
+    "revision": "51ecfb56451154dcc4ba",
+    "url": "/todo_list_updated-axios/static/js/2.b0d3f6b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list_updated-axios/static/js/2.b0d3f6b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df10b8695ae6e3746a4",
+    "url": "/todo_list_updated-axios/static/js/main.f23196a0.chunk.js"
+  },
+  {
+    "revision": "0df7cb78d4d05630b5ec",
+    "url": "/todo_list_updated-axios/static/js/runtime-main.9dfdf312.js"
+  }
+]);
